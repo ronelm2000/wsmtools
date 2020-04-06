5c0e0c938773e371104e8b95d39bfc77021f6d44
ronelm2000 <ronelm2000@gmail.com>
2020-04-06 15:11:34 +0800

    Adds a translation sanity check

---
+++ MontageWeissTools/Impls/Inspectors/Deck/SanityTranslationsInspector.cs
@@ -0,0 +1,34 @@
+﻿using Montage.Weiss.Tools.API;
+using Montage.Weiss.Tools.Entities;
+using Montage.Weiss.Tools.Utilities;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Montage.Weiss.Tools.Impls.Inspectors.Deck
+{
+    public class SanityTranslationsInspector : IExportedDeckInspector
+    {
+        public readonly ILogger Log = Serilog.Log.ForContext<SanityImageInspector>();
+
+        public async Task<WeissSchwarzDeck> Inspect(WeissSchwarzDeck deck, bool isNonInteractive)
+        {
+            var allEmptyTranslations = deck.Ratios.Keys.Where(card => String.IsNullOrWhiteSpace(card.Name.EN))
+                                                        .Select(card => card.ReleaseID)
+                                                        .Distinct();
+
+            if (allEmptyTranslations.Any())
+            {
+                Log.Warning("The following sets (based on Release ID) do not have proper English translations: {allEmptyTranslations}", allEmptyTranslations.ToList());
+                Log.Warning("This may result in a deck generator with only Japanese text.");
+                Log.Warning("Do you wish to continue? [Y/N] (Default is N)");
+                if (ConsoleUtils.Prompted(isNonInteractive))
+                    return deck;
+            }
+            return WeissSchwarzDeck.Empty;
+        }
+    }
+}
---
+++ MontageWeissTools/Utilities/ConsoleUtils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Montage.Weiss.Tools.Utilities
+{
+    static class ConsoleUtils
+    {
+        public static bool Prompted(bool isNonInteractive)
+        {
+            if (isNonInteractive) return false;
+            var result = Console.ReadKey(false).Key == ConsoleKey.Y;
+            Console.Write("\r");
+            return result;
+        }
+    }
+}

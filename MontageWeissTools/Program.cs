fca72100da1c80e3548705ebb3d2d65369d7a194
ronelm2000 <ronelm2000@gmail.com>
2020-07-15 11:27:34 +0800

    Adds version sanity checking Can't be bothered to remove magic words related to the repository for now.

--- MontageWeissTools/Program.cs
+++ MontageWeissTools/Program.cs
@@ -4,12 +4,14 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Montage.Weiss.Tools.API;
 using Montage.Weiss.Tools.Entities;
+using Octokit;
 using Serilog;
 using Serilog.Events;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Montage.Weiss.Tools
@@ -19,6 +21,8 @@ namespace Montage.Weiss.Tools
         public static async Task Main(string[] args)
         {
             Serilog.Log.Logger = BootstrapLogging().CreateLogger();
+
+            await CheckLatestVersion();
 
             Log.Information("Starting...");
 
@@ -30,10 +34,36 @@ namespace Montage.Weiss.Tools
             var verbs = container.GetAllInstances<IVerbCommand>().Select(a => a.GetType()).ToArray();
             var result = CommandLine.Parser.Default.ParseArguments(args, verbs); //
             await result.MapResult<IVerbCommand, Task>(
-                (verb) => verb.Run(container), 
+                (verb) => verb.Run(container),
                 (errors) => Display(errors)
             );
             await Task.CompletedTask;
+        }
+
+        private static async Task CheckLatestVersion()
+        {
+            try
+            {
+                var github = new GitHubClient(new ProductHeaderValue("wsmtools"));
+                //var user = await github.User.Get("ronelm2000");
+                var wsmtoolsLatestRelease = await github.Repository.Release.GetLatest("ronelm2000", "wsmtools");
+                if (wsmtoolsLatestRelease.CreatedAt.DateTime > AppReleaseDate.Subtract(TimeSpan.FromDays(120)))
+                //if (wsmtoolsLatestRelease.TagName != $"v{Program.AppVersion}")
+                {
+                    Log.Information("The latest version is: {version}", wsmtoolsLatestRelease.Name);
+                    Log.Information($"Please see: {wsmtoolsLatestRelease.HtmlUrl}");
+                    Log.Information("If you have any problems with wsmtools, check it out; your issue might have already been fixed!");
+                }
+                else
+                {
+                    Log.Information("The latest version is: {version}", AppVersion);
+                    Log.Information("The latest version on repository: {version}", wsmtoolsLatestRelease.Name);
+                }
+            } catch (Exception e)
+            {
+                Log.Warning("Unable to check the latest version (likely due to internet connection issues).");
+                Log.Warning("If it is, some features of wsmtools may be unusable due to the need of an internet connection.");
+            }
         }
 
         public static LoggerConfiguration BootstrapLogging()
@@ -96,5 +126,10 @@ namespace Montage.Weiss.Tools
             return Task.CompletedTask;
         }
 
+        public static string AppVersion =>
+            Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+
+        public static DateTime AppReleaseDate =>
+            System.IO.File.GetLastWriteTime(Assembly.GetExecutingAssembly().Location);
     }
 }

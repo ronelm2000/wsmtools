5a5e8765da20529cdbcbe34dc4b3bed4180e8c0d
ronelm2000 <ronelm2000@gmail.com>
2020-07-15 13:55:30 +0800

    Adds initial API for adding a database exporter service.

---
+++ MontageWeissTools/API/IDatabaseExporter.cs
@@ -0,0 +1,14 @@
+﻿using Montage.Weiss.Tools.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Montage.Weiss.Tools.API
+{
+    public interface IDatabaseExporter
+    {
+        public string[] Alias { get; }
+        public Task Export(CardDatabaseContext database, IExportInfo info);
+    }
+}

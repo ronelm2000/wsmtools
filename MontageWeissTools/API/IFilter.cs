a4e759ac447d4d4e65e15591d20f290114b10269
ronelm2000 <ronelm2000@gmail.com>
2020-07-20 23:15:42 +0800

    Added a filter so that certain implementations can skip some inpsectors/postprocessors of certain CLIs.

---
+++ MontageWeissTools/API/IFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Montage.Weiss.Tools.API
+{
+    /// <summary>
+    /// This interface signifies that a class that can filter out certain components from executing.
+    /// </summary>
+    public interface IFilter<T>
+    {
+        public bool IsIncluded(T item);
+    }
+}

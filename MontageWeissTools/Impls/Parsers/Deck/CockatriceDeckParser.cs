70540cbe1c23d9080c0702fd96728c05e9d667c8
ronelm2000 <ronelm2000@gmail.com>
2020-11-09 09:49:53 +0800

    Fixes a regression issue caused by a change in COD format. [Cockatrice]

--- MontageWeissTools/Impls/Parsers/Deck/CockatriceDeckParser.cs
+++ MontageWeissTools/Impls/Parsers/Deck/CockatriceDeckParser.cs
@@ -1,7 +1,9 @@
 ﻿using Fluent.IO;
 using Lamar;
+using Microsoft.EntityFrameworkCore.Update;
 using Montage.Weiss.Tools.API;
 using Montage.Weiss.Tools.Entities;
+using Montage.Weiss.Tools.Entities.Exceptions;
 using Montage.Weiss.Tools.Entities.External.Cockatrice;
 using Montage.Weiss.Tools.Impls.Inspectors.Deck;
 using Montage.Weiss.Tools.Utilities;
@@ -13,6 +15,7 @@ using System.IO;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 
@@ -22,6 +25,7 @@ namespace Montage.Weiss.Tools.Impls.Parsers.Deck
     {
         private readonly ILogger Log;
         private Func<CardDatabaseContext> _database;
+        private System.Text.RegularExpressions.Regex serialParser = new System.Text.RegularExpressions.Regex(@"(.*)(?: - )(.*)");
 
         public string[] Alias => new[] { "cockatrice", "cckt3s" };
 
@@ -56,18 +60,37 @@ namespace Montage.Weiss.Tools.Impls.Parsers.Deck
             {
                 var cockatriceDeck = serializer.Deserialize(stream) as CockatriceDeck;
                 var result = new WeissSchwarzDeck();
+                var missingSerials = new List<String>();
                 result.Name = cockatriceDeck.DeckName;
                 result.Remarks = cockatriceDeck.Comments;
-                result.Ratios = cockatriceDeck.Ratios.Ratios.Select(Translate).ToDictionary(p => p.card, p => p.amount);
-                return await Task.FromResult(result);
+                result.Ratios = cockatriceDeck.Ratios.Ratios
+                    .Select(Translate) 
+                    .Select(p =>
+                    {
+                        if (p.card == null)
+                            missingSerials.Add(p.serial);
+                        return p;
+                    })
+                    .Where(p => p.card != null) 
+                    .ToDictionary(p => p.card, p => p.amount);
+
+                if (missingSerials.Count > 0)
+                {
+                    throw new DeckParsingException($"The following cards are missing in the database. Please parse (or re-parse) them again. If you have any Promo Cards, you may need to parse a dedicated promo page first: {JsonSerializer.Serialize(missingSerials)} ");
+                }
+                else
+                {
+                    return await Task.FromResult(result);
+                }
             }
         }
 
-        private (WeissSchwarzCard card, int amount) Translate(CockatriceSerialAmountPair pair)
+        private (string serial, WeissSchwarzCard card, int amount) Translate(CockatriceSerialAmountPair pair)
         {
+            var trueSerial = serialParser.Match(pair.Serial).Groups[1].Value;
             using (var db = _database())
             {
-                return (db.WeissSchwarzCards.Find(pair.Serial), pair.Amount);
+                return (trueSerial, db.WeissSchwarzCards.Find(trueSerial), pair.Amount);
             }
         }
     }

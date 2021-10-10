8f53d4d95f5163b21b140f09dd30cfc5e127b39e
ronelm2000 <ronelm2000@gmail.com>
2021-10-10 14:37:31 +0800

    Fixes Setting.cs warning

--- Montage.Card.API/Entities/Impls/Setting.cs
+++ Montage.Card.API/Entities/Impls/Setting.cs
@@ -8,12 +8,12 @@ namespace Montage.Card.API.Entities.Impls
 {
     public class Setting
     {
-        public string Key { get; set; }
-        public string Value { get; set; }
+        public string? Key { get; set; }
+        public string? Value { get; set; }
 
-        public T GetValue<T>()
+        public T? GetValue<T>()
         {
-            return JsonSerializer.Deserialize<T>(Value);
+            return JsonSerializer.Deserialize<T>(Value ?? throw new NullReferenceException());
         }
 
         public void SetValue<T>(T value)

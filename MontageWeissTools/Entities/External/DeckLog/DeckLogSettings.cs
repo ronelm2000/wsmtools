2a2e51e2c58a9128a8a06babc4e034250c844e11
ronelm2000 <ronelm2000@gmail.com>
2021-10-10 13:49:33 +0800

    Adds DeckPublishURL on DeckLogSettings. Adds sample for exporting DeckLog EN deck links.

--- MontageWeissTools/Entities/External/DeckLog/DeckLogSettings.cs
+++ MontageWeissTools/Entities/External/DeckLog/DeckLogSettings.cs
@@ -15,6 +15,7 @@ namespace Montage.Weiss.Tools.Entities.External.DeckLog
         public string SearchURL { get; set; } = "https://decklog.bushiroad.com/system/app/api/search/2";
         public string CardParamURL { get; set; } = "https://decklog.bushiroad.com/system/app/api/cardparam/2";
         public string DeckViewURL { get; set; } = "https://decklog.bushiroad.com/system/app/api/view/";
+        public string DeckPublishURL { get; set; } = "https://decklog-.bushiroad.com/system/app/api/publish/2";
 
         public static DeckLogSettings Japanese => new();
         public static DeckLogSettings English => new DeckLogSettings
@@ -24,6 +25,7 @@ namespace Montage.Weiss.Tools.Entities.External.DeckLog
             DeckURLMatcher = new Regex(@"(.*):\/\/decklog-en\.bushiroad\.com\/view\/([^\?]*)(.*)"),
             CardParamURL = "https://decklog-en.bushiroad.com/system/app/api/cardparam/2",
             DeckViewURL = "https://decklog-en.bushiroad.com/system/app/api/view/",
+            DeckPublishURL = "https://decklog-en.bushiroad.com/system/app/api/publish/2",
             ImagePrefix = "https://en.ws-tcg.com/wp/wp-content/images/cardimages/",
             Referrer = "https://decklog-en.bushiroad.com/create?c=2",
             SearchURL = "https://decklog-en.bushiroad.com/system/app/api/search/2",

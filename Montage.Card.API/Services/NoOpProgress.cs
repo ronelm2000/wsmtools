cd822da0f99cf59d527cf87f56e477e79a05230b
ronelm2000 <ronelm2000@gmail.com>
2022-04-23 19:27:15 +0800

    Updates all projects to .NET 6 / C# 10.

---
+++ Montage.Card.API/Services/NoOpProgress.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Montage.Card.API.Services
+{
+    /// <summary>
+    /// Represents a Progress class that does nothing.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    internal class NoOpProgress<T> : IProgress<T>
+    {
+        /// <summary>
+        /// Represents a static instance of the class.
+        /// </summary>
+        public static NoOpProgress<T> Instance { get; } = new NoOpProgress<T>();
+
+        private NoOpProgress() { }
+
+        public void Report(T value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MontageWeissTools/GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using Newtonsoft.Json.Linq;
+global using Serilog;
+global using System.Text.RegularExpressions;
+//using System.IO;

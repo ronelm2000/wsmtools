be84551cfe05cc06335df2c792cd1e0eab1b682a
ronelm2000 <ronelm2000@gmail.com>
2022-04-25 23:20:59 +0800

    Adds additonal progress messages for JKTCG PP

--- Montage.Card.API/Entities/Impls/PostProcessorProgressReport.cs
+++ Montage.Card.API/Entities/Impls/PostProcessorProgressReport.cs
@@ -2,10 +2,37 @@
 
 public record PostProcessorProgressReport : UpdateProgressReport
 {
+    public PostProcessorProgressReport ObtainedPostProcessingData(string releaseID)
+    {
+        return this with
+        {
+            Percentage = 10,
+            ReportMessage = new MultiLanguageString
+            {
+                EN = $"Obtained Post-Processing Data for [{releaseID}]"
+            }
+        };
+    }
+
     public PostProcessorProgressReport WithProcessedSerial(ICard card, string postProcessorName)
      => this with
      {
          Percentage = 50,
-         ReportMessage = new MultiLanguageString { EN = $"Post-Processed [{card}] using {postProcessorName}." }
+         ReportMessage = new MultiLanguageString { EN = $"Post-Processed [{card.Serial}] using {postProcessorName}." }
      };
+
+    public PostProcessorProgressReport WithProcessingSerial(ICard card, string postProcessorName)
+     => this with
+     {
+         Percentage = 50,
+         ReportMessage = new MultiLanguageString { EN = $"Post-Processing [{card.Serial}] using {postProcessorName}." }
+     };
+
+    public PostProcessorProgressReport Finished(string postProcessorName)
+     => this with
+     {
+         Percentage = 100,
+         ReportMessage = new MultiLanguageString { EN = $"Finished [{postProcessorName}] Post-Processing." }
+     };
+
 }

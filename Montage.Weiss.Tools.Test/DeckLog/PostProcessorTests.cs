4559a205e555ef875c77cdb5a9b33dbe9765348e
ronelm2000 <ronelm2000@gmail.com>
2022-11-28 21:18:00 +0800

    Updates DeckLog compatibility to 20221117.001

--- Montage.Weiss.Tools.Test/DeckLog/PostProcessorTests.cs
+++ Montage.Weiss.Tools.Test/DeckLog/PostProcessorTests.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Montage.Card.API.Entities.Impls;
 using Montage.Card.API.Services;
+using Montage.Weiss.Tools.CLI;
+using Montage.Weiss.Tools.Entities;
 using Montage.Weiss.Tools.Entities.External.DeckLog;
 using Montage.Weiss.Tools.Impls.PostProcessors;
 using Montage.Weiss.Tools.Test.Commons;
@@ -17,7 +19,6 @@ namespace Montage.Weiss.Tools.Test.DeckLog;
 public class PostProcessorTests
 {
     [TestMethod("DeckLog API Version Test (JP)")]
-    [DeploymentItem("Resources/deck_date_a_live.json")]
     public async Task EnsureLatestVersionJP()
     {
         Serilog.Log.Logger = TestUtils.BootstrapLogging().CreateLogger();
@@ -26,11 +27,10 @@ public class PostProcessorTests
         var deckLogPP = ioc.GetInstance<DeckLogPostProcessor>();
         var settings = DeckLogSettings.Japanese;
         var latestVersion = await deckLogPP.GetLatestVersion(settings);
-        Assert.IsTrue(latestVersion == settings.Version, $"DeckLog API version is outdated; latest version is {latestVersion} need to check for compatibility.");
+        Assert.IsTrue(latestVersion == settings.Version, $"DeckLog API (JP) version is outdated; latest version is {latestVersion} need to check for compatibility.");
     }
 
-    [TestMethod("DeckLog API Version Test (JP)")]
-    [DeploymentItem("Resources/deck_date_a_live.json")]
+    [TestMethod("DeckLog API Version Test (EN)")]
     public async Task EnsureLatestVersionEN()
     {
         Serilog.Log.Logger = TestUtils.BootstrapLogging().CreateLogger();
@@ -39,7 +39,7 @@ public class PostProcessorTests
         var deckLogPP = ioc.GetInstance<DeckLogPostProcessor>();
         var settings = DeckLogSettings.English;
         var latestVersion = await deckLogPP.GetLatestVersion(settings);
-        Assert.IsTrue(latestVersion == settings.Version, $"DeckLog API version is outdated; latest version is {latestVersion} need to check for compatibility.");
+        Assert.IsTrue(latestVersion == settings.Version, $"DeckLog API (EN) version is outdated; latest version is {latestVersion} need to check for compatibility.");
     }
 
     [TestMethod("DeckLog API Set List Test")]
@@ -51,7 +51,10 @@ public class PostProcessorTests
 
         var progress1 = NoOpProgress<SetParserProgressReport>.Instance;
         var progress2 = NoOpProgress<PostProcessorProgressReport>.Instance;
+        var progress3 = NoOpProgress<CommandProgressReport>.Instance;
         var ct = CancellationToken.None;
+
+        await new UpdateVerb().Run(ioc, progress3, ct);
 
         var deckLogPP = ioc.GetInstance<DeckLogPostProcessor>();
         var cards = new Tools.Impls.Parsers.Cards.EncoreDecksParser()
